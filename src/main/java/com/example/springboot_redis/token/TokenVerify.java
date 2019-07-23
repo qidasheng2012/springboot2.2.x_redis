bda46fd7cb90ae599343352a33e84d057091417f
qidasheng2012 <qidasheng2012@163.com>
2019-07-23 17:12:41 +0800

    防重复提交token

---
+++ src/main/java/com/example/springboot_redis/token/TokenVerify.java
@@ -0,0 +1,13 @@
+package com.example.springboot_redis.token;
+
+import java.lang.annotation.*;
+
+/*
+ * 防重复提交注解
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TokenVerify {
+    String value() default "";
+}
