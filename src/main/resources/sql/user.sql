d91c8f1b105d5646b6061b69a7a60498de02b160
qidasheng2012 <qidasheng2012@163.com>
2021-03-25 17:46:20 +0800

    add  init sql

---
+++ src/main/resources/sql/user.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `age` int(3) NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
