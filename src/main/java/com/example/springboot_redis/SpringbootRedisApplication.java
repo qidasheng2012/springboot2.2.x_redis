5fc15318156c28036d23dfd917491495721a97fe
qidasheng2012 <qidasheng2012@163.com>
2021-03-26 11:01:08 +0800

    update pom.xml

--- src/main/java/com/example/springboot_redis/SpringbootRedisApplication.java
+++ src/main/java/com/example/springboot_redis/SpringbootRedisApplication.java
@@ -1,11 +1,13 @@
 package com.example.springboot_redis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.springboot_redis.mapper")
 public class SpringbootRedisApplication {
 
     public static void main(String[] args) {
